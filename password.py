ca6760fa6474bcff3e77d33b2319e963211a2300
annatsai <annatsaianna@gmail.com>
2023-11-25 23:22:22 +0800

    version3

--- password.py
+++ password.py
@@ -1,13 +1,16 @@
 password = 'a12345'
 i = 3#剩餘機會
 while i > 0: 
+	i = i - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登錄成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤,您還有', i ,'次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('您還有', i ,'次機會')
+
 		
 
 
