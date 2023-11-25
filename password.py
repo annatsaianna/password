31445df474e33000b3a46409bb780c164ac5a8ed
annatsai <annatsaianna@gmail.com>
2023-11-25 22:45:42 +0800

    baisic mode'

---
+++ password.py
@@ -0,0 +1,19 @@
+password = 'a12345'
+i = 3#剩餘機會
+while True: 
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登錄成功')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤,您還有', i ,'次機會')
+		if i == 0:
+			break
+
+
+
+
+
+
+
